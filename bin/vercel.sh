688149e0ba271b3ffb3e3cb41a25313d8514d846
CryptoNinja1210 <crypto.ninja1210@gmail.com>
2024-01-04 13:42:13 +0800

    config3

--- bin/vercel.sh
+++ bin/vercel.sh
@@ -12,9 +12,8 @@ rbenv install 3.2.2
 rbenv global 3.2.2
 ruby -v
 
-run ../config.ru
-## project build
-#npm i
-#bundle
-#bundle exec rake bormashino:download
-#(cd src && bundle)
+# project build
+npm i
+bundle
+bundle exec rake bormashino:download
+(cd src && bundle)
