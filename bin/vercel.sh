7410d11855131c58c5d35a0cf04f54f311b92675
CryptoNinja1210 <crypto.ninja1210@gmail.com>
2024-01-04 14:29:00 +0800

    configsh6

--- bin/vercel.sh
+++ bin/vercel.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/bash
 
+yum install -y gcc openssl-devel readline-devel zlib-devel make tar libyaml-devel xz curl
 # rbenv install
-git clone https://github.com/rbenv/rbenv-win.git %USERPROFILE%/.rbenv
-setx PATH "%USERPROFILE%\.rbenv\bin;%USERPROFILE%\.rbenv\shims;%PATH%"
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+(cd ~/.rbenv && src/configure && make -C src)
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+~/.rbenv/bin/rbenv init
+echo 'eval "$(rbenv init - bash)"' >> ~/.bash_profile
+source ~/.bash_profile
 
-rbenv init
-# Load rbenv automatically by appending
-# the following to ~/.zshrc:
-
-eval "$(rbenv init - zsh)"
+mkdir -p "$(rbenv root)"/plugins
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 
 # required ruby install
-rbenv install 3.2.2 | rbenv global 3.2.2
+rbenv rehash
+cat .ruby-version | rbenv install 3.2.2 | rbenv global 3.2.2
 
 # project build
 #npm i
